83e1a81eee17ec44f89d121795051e95d8c56cc9
David Nguyen <tnguyen.david@gmail.com>
2019-11-18 15:38:03 -0800

    Complete login action

--- src/actions/users.js
+++ src/actions/users.js
@@ -12,14 +12,26 @@ const loginSuccess = (data) => {
   };
 }
 
+const loginError = () => {
+  return {
+    type: types.LOGIN_ERROR
+  };
+};
+
 const login = (username, password, successPath) => {
   return dispatch => {
     dispatch({ type: types.LOGIN_USER })
     userService.login(username, password)
     .then((response) => {
       if (response.data.success) {
-        dispatch()
+        dispatch(loginSuccess(data));
+      } else {
+        dispatch(loginError());
+        return response.data.message;
       }
     })
+    .catch((response) => {
+      console.log(`Error ${response.message}`);
+    });
   };
 };
